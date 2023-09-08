5798a345184a34c299dc91def537e1263feafeea
JeongWo <kjw6235@naver.com>
2023-09-08 18:28:21 +0900

    docs: edit

--- join.php
+++ join.php
@@ -5,40 +5,35 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <link rel="stylesheet" href="test.css" />
     </head>
+
     <body>
-    
-    
-    <form action='join_action.php' method='post' > 
-    
-    <div class="row mb-3">
-      <label for="user_id" class="col-sm-2 col-form-label" >Id</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" name="id" id="user_id">
+    <div class = "container">
+    <div class="form-container" id="board-form">
+ 
+    <form action='join_action.php' method='post'> 
+    <div>
+      <label for="user_id">ID</label>
+        <input type="text"  name="id" id="user_id">
         <input type="hidden" name="decide_id" id="decide_id">
         <p><span id="decide" style='color:red;'>ID 중복 여부를 확인해주세요.</span>
-        <input type="button" id="check_button" value="확인하기" onclick="checkid();" class="btn btn-dark" />
-      </div>
+        <input type="button" id="check_button" value="확인하기" onclick="checkid();"  />
     </div>
-    
-    <div class="row mb-3">
-      <label for="pw" class="col-sm-2 col-form-label" >Password</label>
-      <div class="col-sm-10">
-        <input type="password" class="form-control" name="pw" id="pw">
-      </div>
-    </div>
-    
-    <div class="row mb-3">
-      <label for="email" class="col-sm-2 col-form-label" >Email</label>
-      <div class="col-sm-10">
-        <input type="email" class="form-control" name="email" id="email">
-      </div>
-    </div>
-    
-    <fieldset class="row mb-3">
-      </div>
-      <button type="submit" id="join_button" class="btn btn-dark">회원가입</button>
-    </form>
 
+    <div >
+      <label for="pw" >PW</label>
+      <input type="password"  name="pw" id="pw">
+    </div>
+
+    <div >
+      <label for="email" >Email</label>
+      <input type="email" name="email" id="email">
+    </div>
+
+      </div>
+      <button type="submit" id="join_button">회원가입</button>
+    </form>
+</div>
+</div>
   <script>
 
   function checkid(){
