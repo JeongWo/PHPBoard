c2f1c35357d18f71e79aa71e314d6f80c241771c
JeongWo <kjw6235@naver.com>
2023-09-11 17:00:11 +0900

    fix: find_pw.php error fix

--- find_pw.php
+++ find_pw.php
@@ -9,8 +9,8 @@
     <div class="form-container" id="login-form">
     <h1>비밀번호 찾기</h1>
         <form action='find_pw_action.php' method='post'>
-        <label for="id">ID</label>
-        <input type="text" id="id_find" name="id_find" type="text" placeholder="ID를 입력해주세요." />
+        <label>아이디</label>
+        <input type="text" id="id_find" name="id_find" type="text" placeholder="아이디를 입력해주세요." />
         <button type="submit">찾기</button>
         </form>
 </div>
