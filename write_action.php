a4771a66417e2792b9cc9e26235253593abf17ed
JeongWo <kjw6235@naver.com>
2023-09-12 18:55:28 +0900

    feat: image upload

--- write_action.php
+++ write_action.php
@@ -9,7 +9,7 @@ $content = $_POST['content'];
 $date = date('Y-m-d H:i:s');
 $hit = $_POST['hit'];
 
-$target_dir = "uploads/";
+$target_dir = "uploads";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
@@ -20,18 +20,18 @@ if (!isset($_FILES["image"]["error"]) || $_FILES["image"]["error"] == 4) {
   $image_path = "";
 } else {
  
-  if ($_FILES["image"]["size"] > 50000000) {
+  if ($_FILES["image"]["size"] > 5000000) {
       echo "파일이 너무 큽니다.";
       $uploadOk = 0;
   }
 
   if (!in_array($imageFileType, ["jpg", "png", "jpeg", "gif"])) {
       echo "JPG, JPEG, PNG, GIF 형식만 가능합니다.";
-      $uploadOk = 0;
+      $uploadOk = 0; 
   }
 
   if ($uploadOk) {
-      if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+      if (move_uploaded_file($_FILES["image"]["name"], $target_file)) {
           echo "파일 " . htmlspecialchars(basename($_FILES["image"]["name"])) . "이 업로드 되었습니다.";
           $image_path = $target_file; 
           $_SESSION['image_path'] = $image_path; 
