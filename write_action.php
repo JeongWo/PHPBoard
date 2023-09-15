6162a652bac621b9b66cc8c556a73c87e325c9eb
JeongWo <kjw6235@naver.com>
2023-09-15 17:39:00 +0900

    fix : write_action

--- write_action.php
+++ write_action.php
@@ -1,6 +1,11 @@
 <?php
-require_once "connect.php";
+ini_set("display_startup_errors", 1);
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 
+
+
+require_once "connect.php";
 $number = $_GET['number']; 
 $id = $_POST['name'];
 $pw = $_POST['pw'];
@@ -10,22 +15,27 @@ $date = date('Y-m-d H:i:s');
 $hit = $_POST['hit'];
 var_dump($_FILES);
 $target_dir = "uploads/";
+
+//echo 'number ' . $number;
+//echo 'id'.$id;
+
+//$file = $_FIlES["image"];
+
+//var_dump($fime);
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 $uploadOk = 1;
-$imageFileType = mime_content_type($_FILES["image"]["tmp_name"]);
+// $imageFileType = mime_content_type($_FILES["image"]["tmp_name"]);
 
 if ($_FILES["image"]["size"] > 5000000) {
     echo "파일이 너무 큽니다.";
     $uploadOk = 0;
 }
 
-if ($imageFileType !== "image/jpeg" && $imageFileType !== "image/png" && $imageFileType !== "image/gif") {
-    echo "JPG, JPEG, PNG, GIF 형식만 가능합니다.";
-    $uploadOk = 0; 
-}
-ini_set("display_startup_errors", 1);
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
+// if ($imageFileType !== "image/jpeg" && $imageFileType !== "image/png" && $imageFileType !== "image/gif") {
+//     echo "JPG, JPEG, PNG, GIF 형식만 가능합니다.";
+//     $uploadOk = 0; 
+// }
+
 
 if ($uploadOk) {
     if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
